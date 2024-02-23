1b8da41fd359b444252f1148218c5b3d88f9249f
niloykb <niloybarmonvu@gmail.com>
2024-02-23 12:36:54 +0600

    invoice form validation issue fixed

--- src/app/modules/invoice-management/invoice-lists/invoice-form-modal/invoice-form-modal.component.ts
+++ src/app/modules/invoice-management/invoice-lists/invoice-form-modal/invoice-form-modal.component.ts
@@ -33,22 +33,27 @@ export class InvoiceFormModalComponent implements OnInit {
   }
 
   initializeForm() {
+
+    const numberOfDaysToAdd = 7;
+    const currentDate = new Date();
+    const dueDate = currentDate.setDate(currentDate.getDate() + numberOfDaysToAdd);
+
     this.invoiceForm = this.formBuilder.group({
       id: null,
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      address: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      phone: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern('^[0-9]+$')]],
+      address: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(50),]],
       invoice_number: [{ value: '4234', disabled: true }, Validators.required],
-      date: [this.getDefaultDate(), Validators.required],
-      due_date: [this.getDefaultDate(), Validators.required],
+      date: [new Date(), Validators.required],
+      due_date: [dueDate, Validators.required],
       selected_items: [[], Validators.required],
       invoice_items: this.formBuilder.array([this.invoiceItem()]),
-      sub_total: ['', Validators.required],
-      discount: ['', Validators.required],
-      total_amount: [{ value: '', disabled: true }, Validators.required],
-      paid_amount: ['', Validators.required],
-      due_amount: ['', Validators.required],
-      status: ['', Validators.required],
+      sub_total: [null, Validators.required],
+      discount: [null, [Validators.min(5), Validators.max(50), Validators.pattern(/^\d+$/)]],
+      total_amount: [{ value: null, disabled: true }, Validators.required],
+      paid_amount: [null, [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]],
+      due_amount: [null],
+      status: [true, Validators.required],
       remarks: [null, Validators.required],
     });
     this.onChangeSelectedItems();
@@ -58,15 +63,14 @@ export class InvoiceFormModalComponent implements OnInit {
 
   onChangeDiscount() {
     this.invoiceForm.get('discount')?.valueChanges.subscribe((discount) => {
-      const discountAmount = (discount / 100) * this.subTotal;
-      this.invoiceForm.get('total_amount')?.setValue(this.subTotal - discountAmount);
+      this.handleDiscountAmount(discount);
     });
   }
 
   onChangePaidAmount() {
     this.invoiceForm.get('paid_amount')?.valueChanges.subscribe((paid_amount) => {
       if (paid_amount <= this.totalAmount) {
-        const dueAmount = this.totalAmount - paid_amount
+        const dueAmount = this.totalAmount - paid_amount;
         this.invoiceForm.get('due_amount')?.setValue(dueAmount);
       }
     });
@@ -81,7 +85,14 @@ export class InvoiceFormModalComponent implements OnInit {
   }
 
   get dueAmount() {
-    return this.invoiceForm.get('due_amount')?.value;
+    return Math.abs(this.invoiceForm.get('due_amount')?.value);
+  }
+  get discount() {
+    return this.invoiceForm.get('discount');
+  }
+
+  get paidAmount() {
+    return this.invoiceForm.get('paid_amount');
   }
 
   get selectedInvoiceItems() {
@@ -91,7 +102,7 @@ export class InvoiceFormModalComponent implements OnInit {
   onChangeSelectedItems() {
     this.invoiceForm.get('selected_items')?.valueChanges.subscribe((selected_items) => {
       const filteredObjects = this.invoiceItems.filter((item) => selected_items.includes(item.id));
-
+      this.handleDiscountAmount();
       for (let i = this.selectedInvoiceItems.length - 1; i >= 0; i--) {
         const currentItem = this.selectedInvoiceItems.at(i).get('id')?.value;
         if (!filteredObjects.some((item) => item.id === currentItem)) {
@@ -104,7 +115,7 @@ export class InvoiceFormModalComponent implements OnInit {
           this.selectedInvoiceItems.push(this.invoiceItem(item));
         }
       });
-
+      this.calculateTotalAmount();
     });
   }
 
@@ -112,9 +123,9 @@ export class InvoiceFormModalComponent implements OnInit {
     const formGroup = this.formBuilder.group({
       id: data?.id,
       item: [{ value: data?.id, disabled: true }, Validators.required],
-      quantity: [data?.quantity, Validators.required],
+      quantity: [data?.quantity ?? 1, Validators.required],
       price: [data?.price, Validators.required],
-      amount: [data?.amount, Validators.required],
+      amount: [data?.price, Validators.required],
     });
     this.onChangeCalculateItemPrice(formGroup);
     return formGroup;
@@ -122,10 +133,9 @@ export class InvoiceFormModalComponent implements OnInit {
 
   onChangeCalculateItemPrice(formGroup: FormGroup) {
     formGroup.get('quantity')?.valueChanges.subscribe((newQuantity) => {
-
       const price = formGroup.get('price')?.value;
       const newAmount = price * newQuantity;
-
+      
       formGroup.get('amount')?.setValue(newAmount);
       this.calculateTotalAmount();
     });
@@ -135,16 +145,41 @@ export class InvoiceFormModalComponent implements OnInit {
     let subTotal = 0;
 
     this.selectedInvoiceItems.controls.forEach((formGroup) => {
-      const amount = formGroup.get('amount')?.value;
+      const amount = formGroup?.get('amount')?.value;
       subTotal += amount ? amount : 0;
     });
 
     this.invoiceForm.get('sub_total')?.setValue(subTotal);
     this.invoiceForm.get('total_amount')?.setValue(subTotal);
+
+    this.setPaidAmountValidators(subTotal);
+
+    this.handleExistingPaidAmount();
+    this.handleDiscountAmount();
   }
 
-  getDefaultDate() {
-    return new Date().toISOString().slice(0, 10);
+  handleExistingPaidAmount() {
+    const initialPaidAmount = this.paidAmount?.value;
+
+    if (initialPaidAmount) {
+      const initialDueAmount = this.totalAmount - initialPaidAmount;
+      this.invoiceForm.get('due_amount')?.setValue(initialDueAmount);
+
+    }
+  }
+
+  handleDiscountAmount(discount?: number) {
+    const initialDiscount = this.invoiceForm.get('discount')?.value;
+    const discountAmount = ((discount || initialDiscount) / 100) * this.subTotal;
+    const totalPaymentAmount = this.subTotal - discountAmount;
+    this.setPaidAmountValidators(totalPaymentAmount);
+    this.invoiceForm.get('total_amount')?.setValue(totalPaymentAmount);
+    this.handleExistingPaidAmount();
+    this.paidAmount?.updateValueAndValidity();
+  }
+
+  setPaidAmountValidators(paidAmount: number) {
+    this.paidAmount?.setValidators([Validators.min(1), Validators.max(paidAmount), Validators.pattern(/^\d+$/)]);
   }
 
   getActiveInvoiceItems() {
@@ -162,7 +197,7 @@ export class InvoiceFormModalComponent implements OnInit {
   onSubmitForm() {
     this.isDisabled = true;
     this.commonService.onBufferEvent.emit(true);
-    this.dataService.post(this.invoiceForm.value, 'website/book-appointment')
+    this.dataService.post(this.invoiceForm.value, 'website/invoice')
       .subscribe({
         next: (response) => {
           this.commonService.onBufferEvent.emit(false);
